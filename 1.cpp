21a80fd76fc1225519f882d14428b7f38632a8f4
Nainaaaa <dasnaina378@gmail.com>
2018-04-15 08:33:50 +0000

    first commit

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"enter the number :";
+  cin>>a;
+  for(int i=1;i<=a;i++)
+  {
+   cout<<"execute " << i  << " times ";
+    cout<<"\n";
+  }
+ 
+  return 1;
+}
